400f9d83c8d5bdd72afa05164088e6bfb8670e0b
Lasermancer <85950677+Lasermancer0@users.noreply.github.com>
2022-04-25 01:08:44 +0200

    Add files via upload

--- FortCard/Client_GameRefresh.lua
+++ FortCard/Client_GameRefresh.lua
@@ -1,5 +1,9 @@
 function Client_GameRefresh(game)
 	local playerData = Mod.PlayerGameData;
+    if (game.Us == nil) then
+        UI.Alert("You can't do anything as a spectator until the game has ended.")
+        return
+    end
 	if (playerData.otherCardsCount ~= nil and playerData.otherCardsCount ~= -1) then
 		if (playerData.numCards + playerData.otherCardsCount > game.Settings.MaxCardsHold) then
 			UI.Alert("You can hold only "..game.Settings.MaxCardsHold.. " cards. If you fail to use ".. (playerData.numCards + playerData.otherCardsCount - game.Settings.MaxCardsHold) .. " cards, appropriate number of special cards will be discarded.");
