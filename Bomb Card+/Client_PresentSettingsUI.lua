719b9135db5e2b63de06c657faf89f1ed97264d1
Lasermancer <85950677+Lasermancer0@users.noreply.github.com>
2022-04-30 08:13:30 +0200

    Update Client_PresentSettingsUI.lua

--- Bomb Card+/Client_PresentSettingsUI.lua
+++ Bomb Card+/Client_PresentSettingsUI.lua
@@ -3,15 +3,15 @@ function Client_PresentSettingsUI(rootParent)
 	if (Mod.Settings.killPercentage == 0) then
 		if (Mod.Settings.delayed) then
 			if (Mod.Settings.armiesKilled == 1) then
-				UI.CreateLabel(rootParent).SetText("The bomb card kills one enemy troop. If all troops are killed, targeted territory turns neutral. The effect happens at the end of your turn.");
+				UI.CreateLabel(rootParent).SetText("The bomb card kills one enemy troop. The effect happens at the end of your turn.");
 			else
-				UI.CreateLabel(rootParent).SetText("The bomb card kills ".. Mod.Settings.armiesKilled.." enemy troops. If all troops are killed, targeted territory turns neutral. The effect happens at the end of your turn.");
+				UI.CreateLabel(rootParent).SetText("The bomb card kills ".. Mod.Settings.armiesKilled.." enemy troops. The effect happens at the end of your turn.");
 			end
 		else
 			if (Mod.Settings.armiesKilled == 1) then
-				UI.CreateLabel(rootParent).SetText("The bomb card kills one troop. If all troops are killed, targeted territory turns neutral.");
+				UI.CreateLabel(rootParent).SetText("The bomb card kills one troop.");
 			else
-			UI.CreateLabel(rootParent).SetText("The bomb card kills ".. Mod.Settings.armiesKilled.." troops. If all troops are killed, target territory turns neutral.");
+			UI.CreateLabel(rootParent).SetText("The bomb card kills ".. Mod.Settings.armiesKilled.." troops.");
 			end
 		end
 	else
@@ -23,13 +23,13 @@ function Client_PresentSettingsUI(rootParent)
 		UI.CreateLabel(rootParent).SetText("Note, that this rounds up (if the card were to kill 0.5 troops, it would have killed 1 troop).");
 		if (Mod.Settings.armiesKilled ~= 0) then
 			if (Mod.Settings.armiesKilled == 1) then
-				UI.CreateLabel(rootParent).SetText("After that, the card kills one additional troop. If all troops are killed, targeted territory turns neutral.");
+				UI.CreateLabel(rootParent).SetText("After that, the card kills one additional troop.");
 			else
-			UI.CreateLabel(rootParent).SetText("After that, the card kills ".. Mod.Settings.armiesKilled.." additional troops. If all troops are killed, targeted territory turns neutral.");
+			UI.CreateLabel(rootParent).SetText("After that, the card kills ".. Mod.Settings.armiesKilled.." additional troops.");
 			end
 		end
 	end
-	UI.CreateLabel(rootParent).SetText("This mod will create fake discard orders.");
+	UI.CreateLabel(rootParent).SetText("If all troops are killed, targeted territory turns neutral. This mod will create fake discard orders.");
 
 end
 
